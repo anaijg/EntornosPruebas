7ff740479baefbd43229c51beb94bee7a627c2a1
AnaIJim <ana.jimenezgomez@educa.madrid.org>
2024-04-16 18:05:52 +0200

    16-04-2024

--- src/main/java/ejercicioscajanegra/ejercicio1/Excursion.java
+++ src/main/java/ejercicioscajanegra/ejercicio1/Excursion.java
@@ -17,10 +17,26 @@ public class Excursion {
 
     // métodos de instancia
     public double calcularPrecioAlumno() {
-        return 0;
+        if (numeroAlumnos >= 100) {
+            return 65;
+        } else if (numeroAlumnos >= 50 && numeroAlumnos < 100) {
+            return 70;
+        } else if (numeroAlumnos >= 30 && numeroAlumnos < 50) {
+            return 95;
+        } else {
+            return 4000/numeroAlumnos;
+        }
     }
 
     public double calcularCosteExcursion() {
-        return 0;
+        if (numeroAlumnos >= 100) {
+            return 65 * numeroAlumnos;
+        } else if (numeroAlumnos >= 50 && numeroAlumnos < 100) {
+            return 70 * numeroAlumnos;
+        } else if (numeroAlumnos >= 30 && numeroAlumnos < 50) {
+            return 95 * numeroAlumnos;
+        } else {
+            return 4000;
+        }
     }
 }
--- src/main/java/ejercicioscajanegra/ejercicio1/ExcursionMetodosEstaticos.java
+++ src/main/java/ejercicioscajanegra/ejercicio1/ExcursionMetodosEstaticos.java
@@ -2,11 +2,27 @@ package ejercicioscajanegra.ejercicio1;
 
 public class ExcursionMetodosEstaticos {
     public static double calcularPrecioAlumno(int numeroAlumnos) {
-        return 0;
+        if (numeroAlumnos >= 100) {
+            return 65;
+        } else if (numeroAlumnos >= 50 && numeroAlumnos < 100) {
+            return 70;
+        } else if (numeroAlumnos >= 30 && numeroAlumnos < 50) {
+            return 95;
+        } else {
+            return 4000/numeroAlumnos;
+        }
     }
 
     public static double calcularCosteExcursion(int numeroAlumnos) {
-        return 0;
+        if (numeroAlumnos >= 100) {
+            return 65 * numeroAlumnos;
+        } else if (numeroAlumnos >= 50 && numeroAlumnos < 100) {
+            return 70 * numeroAlumnos;
+        } else if (numeroAlumnos >= 30 && numeroAlumnos < 50) {
+            return 95 * numeroAlumnos;
+        } else {
+            return 4000;
+        }
     }
 
 }
