6f7c405384cced53e5b3f747de43c1119c83e3aa
anaijg <ana.jimenezgomez@educa.madrid.org>
2024-04-26 14:36:34 +0200

    Añado paquete empresa para javadoc

---
+++ src/main/java/javadoc/empresa/Directivo.java
@@ -0,0 +1,59 @@
+package javadoc.empresa;
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
+/**
+ * Clase Directivo, que
+ * hereda de Empleado.
+ * Tiene el atributo categoría, que es un tipo enumerado cuyos valores obtendremos de la siguiente URL: https://economipedia.com/definiciones/directivo.html
+ * También sobreescribe el método mostrar con los valores de todos los atributos heredados más el atributo categoría.
+ * Su constructor recibe todos los atributos como parámetros
+ */
+
+public class Directivo extends Empleado{
+    private Categoria categoria;
+
+    public Directivo(String nombre, LocalDate fechaNacimiento, double sueldoBruto, Categoria categoria) {
+        super(nombre, fechaNacimiento, sueldoBruto);
+        this.categoria = categoria;
+    }
+
+    @Override
+    public String toString() {
+        return "Directivo{" +
+                "categoria=" + categoria +
+                ", sueldoBruto=" + sueldoBruto +
+                '}';
+    }
+
+    @Override
+    public void mostrar() {
+        System.out.println("\n\nDATOS DEL DIRECTIVO: ");
+        super.mostrar();
+        System.out.println(this.toString());
+    }
+
+    // LOS DIRECTIVOS COBRAN UN PLUS POR EL HECHO DE SERLO. ADEMÁS, SE ASIGNA, A DEDO, ES DECIR, SE PIDE POR TECLADO: AÑADIR MÉTODOS: ASIGNAR PLUS, QUE INCREMENTA EL SALARIO BRUTO EN EL PLUS QUE PIDAMOS POR TECLADO (PEDIRLO EN OTRO MÉTODO: PEDIRPLUS(): DOUBLE). SOBREESCRIBIR EL MÉTODO CALCULAR SALARIO NETO PORQUE POR INGENIERÍA FINANCIERA SÓLO SE DESCUENTAN UN 5% DEL SALARIO BRUTO.
+    public void asignarPlus() {
+        double plus = pedirPlus();
+        sueldoBruto = sueldoBruto + plus;
+    }
+
+    // PIDAMOS POR TECLADO (PEDIRLO EN OTRO MÉTODO: PEDIRPLUS(): DOUBLE)
+    // private porque solamente quiero que sea llamado dentro de esta clase, desdel el método asignarPlus()
+    private double pedirPlus() {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Introduce plus: ");
+        return teclado.nextDouble();
+    }
+
+    // CALCULAR SALARIO NETO PORQUE POR INGENIERÍA FINANCIERA SÓLO SE DESCUENTAN UN 5% DEL SALARIO BRUTO.
+
+
+    @Override
+    public double calcularSalarioNeto() {
+        double salarioNeto = sueldoBruto - sueldoBruto * 5 / 100;
+        return salarioNeto;
+    }
+}
