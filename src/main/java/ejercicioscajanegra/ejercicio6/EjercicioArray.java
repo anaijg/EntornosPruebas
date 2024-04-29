0d5e8095956d18ea2cfe1ce9ab7553d59dff74b8
anaijg <ana.jimenezgomez@educa.madrid.org>
2024-04-29 14:48:50 +0200

    'Soluciones'

--- src/main/java/ejercicioscajanegra/ejercicio6/EjercicioArray.java
+++
@@ -1,5 +0,0 @@
-package ejercicioscajanegra.ejercicio6;
-
-public class EjercicioArray {
-
-}
---
+++ src/test/java/ejercicioscajanegra/ejercicio3/Ejercicio3Test.java
@@ -0,0 +1,13 @@
+package ejercicioscajanegra.ejercicio3;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Ejercicio3Test {
+
+    @Test
+    void sumarDel1al10() {
+        assertEquals(55, Ejercicio3.sumarDel1al10());
+    }
+}
